cd4ba4ba51f9b3fab061bf2e24e483b4aa19b1bd
Electrical <electrical-neo@proton.me>
2022-09-25 16:27:50 +0200

    Building the floppy disk image

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 
 ROOT ?= $(shell pwd)
 
+.PHONY: all clean run
+
 all: run
 
 include boot/Makefile
@@ -8,5 +10,11 @@ include boot/Makefile
 clean:
 	-rm -rf build
 
-run: $(ROOT)/build/boot/stage1.bin
+Starstone.img: $(ROOT)/build/boot/stage1.bin
+	dd if=/dev/zero of=$@ bs=8192 count=180 # 8192 * 180 = 1440k
+	mformat -i $@ -f 1440 ::
+	dd if=$(ROOT)/build/boot/stage1.bin of=Starstone.img bs=1 count=3 conv=notrunc
+	dd if=$(ROOT)/build/boot/stage1.bin of=Starstone.img bs=1 count=448 seek=62 skip=62 conv=notrunc
+
+run: Starstone.img
 	qemu-system-i386 -fda $<
--- boot/stage1.asm
+++ boot/stage1.asm
@@ -1,8 +1,32 @@
 [bits 16]
 [org 0x7c00]
 
-jmp 0:boot
-boot:
+jmp boot
+nop
+
+bpb:
+.oem: db "12345678"
+.bytes_per_sector: dw 0
+.sectors_per_cluster: db 0
+.reserved_sectors: dw 0
+.fats: db 0
+.root_dir_entries: dw 0
+.total_sectors: dw 0
+.media: db 0
+.sectors_per_fat: dw 0
+.sectors_per_track: dw 0
+.heads: dw 0
+.hidden_sectors: dd 0
+.large_sector_count: dd 0
+.drive: db 0
+.nt_flags: db 0
+.signature: db 0
+.serial: dd 0
+.volume_label: db "12345678901"
+.system_id: db "12345678"
+
+boot: jmp word 0:start
+start:
 	mov ah, 0xe
 	mov al, 'A'
 	xor bx, bx
