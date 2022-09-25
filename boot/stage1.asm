5a24b10bbb275c03893852f370cc711140eaf0ae
Electrical <electrical-neo@proton.me>
2022-09-25 17:02:16 +0200

    Added print function

--- boot/stage1.asm
+++ boot/stage1.asm
@@ -27,11 +27,44 @@ bpb:
 
 boot: jmp word 0:start
 start:
-	mov ah, 0xe
-	mov al, 'A'
-	xor bx, bx
-	int 0x10
+	
+	; SET SEGMENT REGISTERS AND STACK POINTER
+	
+	xor ax, ax
+	mov ds, ax
+	mov es, ax
+	
+	mov ax, 0xffff
+	mov sp, 0
+	
+	mov si, data.booting_up
+	call print
+	
 	jmp $
+
+; INPUT ds:si - string
+print:
+	push ax
+	push bx
+	pushf
+	
+	mov ah, 0xe
+	xor bx, bx
+.loop:
+	lodsb
+	or al, al
+	jz .end
+	int 0x10
+	jmp .loop
+.end:
+	popf
+	pop bx
+	pop ax
+	
+	ret
+
+data:
+.booting_up: db "Booting up Starstone 1.0...", 13, 10, 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
